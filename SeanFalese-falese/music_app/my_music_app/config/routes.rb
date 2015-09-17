91ce3abbf4666231e13e1a5971a2542d2b858ae4
falese <falese@gmail.com>
2015-09-16 19:20:51 -0700

    commit of updated static_pages, routes, bootstrap JS library, custom CSS

--- SeanFalese-falese/music_app/my_music_app/config/routes.rb
+++ SeanFalese-falese/music_app/my_music_app/config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/test'
+  #establish a custom route
+  get 'about', to: "static_pages#about"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  # You can have the root of your site routed with "root and thats what i did below"
+  root 'static_pages#home'
   resources :artists
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
