7a56a046550b586815224eea622ee29eac509f7f
falese <falese@gmail.com>
2015-09-16 18:05:24 -0700

    commit of artist, and song controllers

---
+++ SeanFalese-falese/music_app/my_music_app/app/controllers/song_controller.rb
@@ -0,0 +1,16 @@
+class SongController < ApplicationController
+  def index
+  end
+  def show
+  end
+  def new
+    @artist = Artist.new
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end
+
+end
