2ac8bb9ccdf97e45b2911ed553070023fdd5c9b2
falese <falese@gmail.com>
2015-09-16 18:29:53 -0700

    commit of new-edit artist partial and updated artist controller

--- SeanFalese-falese/music_app/my_music_app/app/controllers/artists_controller.rb
+++ SeanFalese-falese/music_app/my_music_app/app/controllers/artists_controller.rb
@@ -1,17 +1,34 @@
 class ArtistsController < ApplicationController
 
   def index
+    @artists = Artist.all
   end
   def show
+    @artist = Artist.find(params[:id])
   end
   def new
-    @artist = Artist.new
   end
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to @artist
+    else
+      render 'new'
+    end
+  end
+
   def edit
+
   end
+
   def update
   end
   def destroy
+  end
+private
+  def artist_params
+    params.require(:name).permit(:description, :image_url)
+
   end
 
 end
