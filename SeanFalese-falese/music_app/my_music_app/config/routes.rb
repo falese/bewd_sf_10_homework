8bdfe4cada0e559add63f7b6d1a4b8c373d7545a
falese <falese@gmail.com>
2015-09-16 19:33:57 -0700

    commit of updated styling and routes

--- SeanFalese-falese/music_app/my_music_app/config/routes.rb
+++ SeanFalese-falese/music_app/my_music_app/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
 
-  get 'static_pages/about'
+  # get 'static_pages/about'
 
-  get 'static_pages/test'
+
   #establish a custom route
   get 'about', to: "static_pages#about"
+  get 'test', to: "static_pages#test"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
