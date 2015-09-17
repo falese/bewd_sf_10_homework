8f15a4d7dc5a435bd0fc59452dcb387549740f6e
falese <falese@gmail.com>
2015-09-16 21:13:17 -0700

    completed CRUD methods for artists controller, added all views, created form partial for edit and create

--- SeanFalese-falese/music_app/my_music_app/app/controllers/artists_controller.rb
+++ SeanFalese-falese/music_app/my_music_app/app/controllers/artists_controller.rb
@@ -7,27 +7,39 @@ class ArtistsController < ApplicationController
     @artist = Artist.find(params[:id])
   end
   def new
+    @artist = Artist.new
   end
   def create
     @artist = Artist.new(artist_params)
-    if @artist.save
-      redirect_to @artist
+     @artist.save
+     if @artist.save
+      redirect_to artists_path
     else
       render 'new'
     end
   end
 
   def edit
-
+    @artist = Artist.find(params[:id])
   end
 
   def update
+    @artist = Artist.find(params[:id])
+    if @artist.update(artist_params)
+      redirect_to artists_path
+    else
+      render 'edit'
+    end
   end
   def destroy
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+    redirect_to artists_path
+
   end
 private
   def artist_params
-    params.require(:name).permit(:description, :image_url)
+    params.require(:artist).permit(:name, :description, :image_url)
   end
 
 end
