0920973a33d8843cda48c9c75bd888aeb194716b
falese <falese@gmail.com>
2015-09-21 19:46:15 -0700

    integration of custom js libraries for theme, and update of js application

--- SeanFalese-falese/music_app/my_music_app/app/assets/javascripts/application.js
+++ SeanFalese-falese/music_app/my_music_app/app/assets/javascripts/application.js
@@ -11,7 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.easing.min
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require grayscale
 //= require turbolinks
 //= require_tree .
